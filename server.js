c2f09f9f1d33a9975bf13ecfcbbeadc93faf7f54
belunatic <abelluvyou@yahoo.com>
2025-12-05 13:40:08 -0800

    update the cors origin, token expiration time

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 // ========= Middlewares =================
 app.use(morgan("dev")); // logger
 app.use(express.json()); // body parser
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 require("./config/passport");
 
 // ========= Routes ======================
